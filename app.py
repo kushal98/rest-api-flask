b4698465567a48bf5c61b16b18f647fa719411b2
Kushal Agrawal <kagrawal61@gmail.com>
2018-09-04 01:01:16 +0530

    getting json data

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask , jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+stores=[
+    {
+        'name' : 'My store',
+        'item':[
+            {
+                'name' : 'first item',
+                'price': 10.09
+            }
+        ]
+    }
+]
 
-def home():
-    return "Hello! world"
+#POST /store 
+@app.route('/',methods=['POST'])
+def create_store():
+    pass
+#GET /store
+@app.route('/store',methods=['GET'])
+def get_stores():
+    return jsonify({'stores':stores})
 
+#GET /store/<string:name>
+@app.route('/store/<string:name>',methods=['GET'])
+def get_store(name):
+    pass
+#POST /store/<string :name>/item {name:,price:}
+@app.route('/store/<string:name>/item')
+def add_item_in_store(name):
+    pass
+#GET /store/<string : name>/item
+@app.route('/store/<string:name>/item')
+def get_item_from_store(name):
+    pass
 app.run(port=5000)
