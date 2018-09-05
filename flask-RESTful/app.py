05a62681e97f51520851bc4429b3f8f17717bdd8
Kushal Agrawal <kagrawal61@gmail.com>
2018-09-06 02:08:57 +0530

    making Item

---
+++ flask-RESTful/app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request
+from flask_restful import Api,Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self,name):
+        for item in items:
+            if item['name']==name:
+                return item
+        return {'item':None},404
+    
+    def post(self,name):
+        data = request.get_json()
+        item = {'name':name,'price':12.00}
+        items.append(item)
+        return item,201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+    
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/item')
+app.run(port=5000,debug=True)
